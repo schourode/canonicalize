4f97e932629891deb213647016abd195541eb5e8
schourode <jsr@malamute.dk>
2011-11-16 16:48:21 +0100

    Add XML documentation to extension method HttpRequestBase.GetOriginalUrl() and make the method public.

--- Canonicalize/HttpRequestBaseExtensions.cs
+++ Canonicalize/HttpRequestBaseExtensions.cs
@@ -3,8 +3,17 @@ using System.Web;
 
 namespace Canonicalize
 {
-    internal static class HttpRequestBaseExtensions
+    /// <summary>
+    /// Adds extension methods on <see cref="HttpRequestBase"/>.
+    /// </summary>
+    public static class HttpRequestBaseExtensions
     {
+        /// <summary>
+        /// Gets the original URL requested by the client, without artifacts from proxies or load balancers.
+        /// In particular HTTP headers Host, X-Forwarded-Host, X-Forwarded-Proto are applied on top of <see cref="HttpRequestBase.Url"/>.
+        /// </summary>
+        /// <param name="request">The request for which the original URL should be computed.</param>
+        /// <returns>The original URL requested by the client.</returns>
         public static Uri GetOriginalUrl(this HttpRequestBase request)
         {
             if (request.Url == null || request.Headers == null)
